6ee10cf6f2ca8c1dc8ae79bc719b17013233af69
Ebenaruto <omartial30@gmail.com>
2020-06-05 03:12:34 +0000

    menu et footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<hr>
+<div class="foot">
+    <div><p>©Copyright Simplon-Burkina, Juin 2020.</p></div>
+
+    <div class="maricon">
+        <img src="assets/img/face.png" href="www.facebook.com" alt="facebook">
+        <img src="assets/img/whatsapp.png" href="www.whatsapp.com" alt="whatsapp">
+        <img src="assets/img/youtube.png" href="www.youtube.com" alt="youtube">
+        <img src="assets/img/git.png" href="www.github.com" alt="github">
+    </div>
+</div>
+
+</body>
+</html>
